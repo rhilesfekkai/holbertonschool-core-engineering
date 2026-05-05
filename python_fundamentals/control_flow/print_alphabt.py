3fda9ddfb83c78a8f4452232507f0c85d9bc5f8a
rhilesfekkai <ramirhiles@gmail.com>
2026-05-05 18:23:40 +0000

    Final fix print_alphabt

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
 for c in range(ord('a'), ord('z') + 1):
-    if chr(c) != 'q' and chr(c) != 'e':
+    if c != ord('q') and c != ord('e'):
         print("{}".format(chr(c)), end="")
-print()
