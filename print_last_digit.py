9fb4987b06a6d88e6385a2fb6a8fe944bc6e78cd
rhilesfekkai <ramirhiles@gmail.com>
2026-05-05 18:51:30 +0000

    Fix print_last_digit logic (checker fix)

--- print_last_digit.py
+++ print_last_digit.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
 def print_last_digit(number):
-    last = number % 10
-    if last < 0:
-        last = -last
+    last = abs(number) % 10
     print("{}".format(last), end="")
     return last
