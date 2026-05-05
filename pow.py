a0e4f35c0ca30005e2e30b47d23ee5ad99a8ac16
rhilesfekkai <ramirhiles@gmail.com>
2026-05-05 18:54:32 +0000

    Fix pow with negative exponent support

---
+++ pow.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def pow(a, b):
+    result = 1
+
+    if b >= 0:
+        for _ in range(b):
+            result *= a
+        return result
+
+    for _ in range(-b):
+        result *= a
+
+    return 1 / result
