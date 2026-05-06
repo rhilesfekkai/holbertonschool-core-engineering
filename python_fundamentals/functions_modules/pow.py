08c4091ebe1f7d5916c9204c98275fb36d5837b8
rhilesfekkai <ramirhiles@gmail.com>
2026-05-06 11:10:19 +0000

    fix pow function

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
-
 def pow(a, b):
     result = 1
-    for _ in range(b):
+
+    if b == 0:
+        return 1
+
+    if b < 0:
+        b = -b
+        for i in range(b):
+            result *= a
+        return 1 / result
+
+    for i in range(b):
         result *= a
+
     return result
