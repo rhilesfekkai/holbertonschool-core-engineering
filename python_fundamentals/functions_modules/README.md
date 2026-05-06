dcf36fc93a26adbb233867c0eb72318db7267d52
rhilesfekkai <ramirhiles@gmail.com>
2026-05-06 11:35:27 +0000

    fix add import task with constraints

---
+++ python_fundamentals/functions_modules/README.md
@@ -0,0 +1,1 @@
+Python import functions tasks
--- python_fundamentals/functions_modules/add.py
+++ python_fundamentals/functions_modules/add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from add_0 import add
 
+a = 1
+b = 2
+
 if __name__ == "__main__":
-    a = 1
-    b = 2
-    result = add(a, b)
-    print(f"{a} + {b} = {result}")
+    print("{} + {} = {}".format(a, b, add(a, b)))
