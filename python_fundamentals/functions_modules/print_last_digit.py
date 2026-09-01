da966e63a8fca749bb07ea8a667bbbf920c6d0bc
rhilesfekkai <ramirhiles@gmail.com>
2026-09-01 07:47:12 +0000

    Fix print last digit function

--- python_fundamentals/functions_modules/print_last_digit.py
+++ python_fundamentals/functions_modules/print_last_digit.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
-
 def print_last_digit(number):
-    last = number % 10
-    if last < 0:
-        last = -last
-    print("{}".format(last), end="")
-    return last
+    last_digit = abs(number) % 10
+    print(last_digit, end="")
+    return last_digit
