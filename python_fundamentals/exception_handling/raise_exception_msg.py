9a10c728c55641fb623a16170dfee23fa13c5b5f
rhilesfekkai <ramirhiles@gmail.com>
2026-09-10 12:40:02 +0000

    Add raise exception message function

---
+++ python_fundamentals/exception_handling/raise_exception_msg.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Raise a NameError with a message."""
+
+
+def raise_exception_msg(message=""):
+    """Raise a NameError with the given message."""
+    raise NameError(message)
