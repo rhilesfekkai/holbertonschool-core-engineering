80a029feb397139cec7a537b784806bed45b1e83
rhilesfekkai <ramirhiles@gmail.com>
2026-09-10 12:37:36 +0000

    Fix safe list integer handling

--- python_fundamentals/exception_handling/safe_print_list_integers.py
+++ python_fundamentals/exception_handling/safe_print_list_integers.py
@@ -3,16 +3,15 @@
 
 
 def safe_print_list_integers(my_list=[], x=0):
-    """Print integers from the first x elements and return their count."""
+    """Print integers from the first x elements."""
     count = 0
 
     for i in range(x):
         try:
-            if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i]), end="")
-                count += 1
-        except IndexError:
-            break
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            pass
 
     print()
     return count
