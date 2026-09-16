fef7f02152afb52b46726887506e587826e6fd33
rhilesfekkai <ramirhiles@gmail.com>
2026-09-16 11:50:04 +0000

    Add getter and setter for Square size

---
+++ python_oop/classes_and_object_model/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Defines a Square class."""
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        """Initialize a square with a size."""
+        self.size = size
+
+    @property
+    def size(self):
+        """Return the square size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the square size after validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current square area."""
+        return self.__size ** 2
