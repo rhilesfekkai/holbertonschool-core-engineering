a041d3bc4c5fde174b2f50eb68fa15e293cedeb9
rhilesfekkai <ramirhiles@gmail.com>
2026-09-16 12:02:30 +0000

    Add Rectangle class

---
+++ python_oop/inheritance/1-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Defines the Rectangle class."""
+
+BaseGeometry = __import__("base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Represents a rectangle."""
+
+    def __init__(self, width, height):
+        """Initialize a rectangle with width and height."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
